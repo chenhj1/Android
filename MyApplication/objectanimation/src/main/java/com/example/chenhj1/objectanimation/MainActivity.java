dea830713064eee466bb4feeb52114fea9f64549
陈浩杰 <chenhj1@ifeng.com>
2016-07-27 15:14:22 +0800

    接着修改gitignore文件，删掉名为download的moudel

--- MyApplication/objectanimation/src/main/java/com/example/chenhj1/objectanimation/MainActivity.java
+++ MyApplication/objectanimation/src/main/java/com/example/chenhj1/objectanimation/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.chenhj1.objectanimation;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +30,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = (ImageView)findViewById(R.id.imageview);
+        textView = (TextView)findViewById(R.id.textview);
+        AnimatorSet set = new AnimatorSet();
+        ObjectAnimator rotateAnim = ObjectAnimator.ofFloat(textView,"scaleX",1f,1.5f);
+        rotateAnim.setRepeatMode(Animation.REVERSE);
+        rotateAnim.setRepeatCount(-1);
 
-        imageView.setPivotX(0);
+        ObjectAnimator rotateAnim2 = ObjectAnimator.ofFloat(textView,"scaleY",1f,1.5f);
+        rotateAnim2.setRepeatMode(Animation.REVERSE);
+        rotateAnim2.setRepeatCount(-1);
+
+        set.playTogether(rotateAnim,rotateAnim2);
+        set.setDuration(1000);
+        set.start();
+
+        /*imageView.setPivotX(0);
         imageView.setPivotY(0);
         ObjectAnimator anim = getScaleAnim();
-        setAnimDetail(anim);
+        setAnimDetail(anim);*/
 
     }
 
@@ -37,13 +57,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private ObjectAnimator getScaleAnim(){
-        ObjectAnimator scaleAnim = ObjectAnimator.ofFloat(imageView,"scaleX",0f,1.2f,1.0f,1.5f,1.0f);
+        ObjectAnimator scaleAnim = ObjectAnimator.ofFloat(imageView,"scaleXY",0f,1.2f,1.0f,1.5f,1.0f);
         return scaleAnim;
     }
 
-    private ObjectAnimator getBackgroundAnim(){
+    /*private ObjectAnimator getBackgroundAnim(){
         ObjectAnimator
-    }
+    }*/
 
     private void setAnimDetail(ObjectAnimator anim){
         anim.setDuration(5000);
--- MyApplication/shimmertextview/src/main/java/com/example/chenhj1/shimmertextview/MainActivity.java
+++ MyApplication/shimmertextview/src/main/java/com/example/chenhj1/shimmertextview/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.chenhj1.shimmertextview;
 
 import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
 
 import com.romainpiel.shimmer.Shimmer;
 import com.romainpiel.shimmer.ShimmerButton;
@@ -25,9 +28,22 @@ public class MainActivity extends AppCompatActivity {
 
 
         //可以通过shimmer控制闪闪发亮动画
-        shimmer.setRepeatCount(20)
+        shimmer.setRepeatCount(-1)
                 .setDuration(1000)
                 .setStartDelay(300)
                 .setDirection(Shimmer.ANIMATION_DIRECTION_LTR);
+
+        AnimatorSet set = new AnimatorSet();
+        ObjectAnimator rotateAnim = ObjectAnimator.ofFloat(shimmerTextView,"scaleX",1f,1.2f);
+        rotateAnim.setRepeatMode(Animation.REVERSE);
+        rotateAnim.setRepeatCount(-1);
+
+        ObjectAnimator rotateAnim2 = ObjectAnimator.ofFloat(shimmerTextView,"scaleY",1f,1.2f);
+        rotateAnim2.setRepeatMode(Animation.REVERSE);
+        rotateAnim2.setRepeatCount(-1);
+
+        set.playTogether(rotateAnim,rotateAnim2);
+        set.setDuration(1000);
+        set.start();
     }
 }
